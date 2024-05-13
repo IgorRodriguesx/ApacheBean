f176434496f15bbb29cb9f3bb979bd26b6adb31c
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-05-13 17:05:13 -0300

    Update ComandosPython.py

--- ComandosPython.py
+++ ComandosPython.py
@@ -1,5 +1,8 @@
 import apache_beam as beam
+from apache_beam.io import ReadFromText
 from apache_beam.options.pipeline_options import PipelineOptions
 
 pipeline_options = PipelineOptions(argv=none)
 pipeline = beam.Pìpeline(options = pipeline_options)
+
+dengue = ( pipeline | "Leitura do Dataset dos casos de Dengue" >> ReadFromText())
