5dcbf961feffc888c246e1f233ba79aea79fe1d3
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-05-15 10:31:24 -0300

    Update ComandosPython.py

--- ComandosPython.py
+++ ComandosPython.py
@@ -1,8 +1,16 @@
+# Importa as bibliotecas necessárias do Apache Beam para processamento de dados em pipeline.
 import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.options.pipeline_options import PipelineOptions
 
-pipeline_options = PipelineOptions(argv=none)
-pipeline = beam.Pìpeline(options = pipeline_options)
+# Configura as opções do pipeline.
+pipeline_options = PipelineOptions(argv=None)
 
-dengue = ( pipeline | "Leitura do Dataset dos casos de Dengue" >> ReadFromText())
+# Inicializa o pipeline.
+pipeline = beam.Pipeline(options=pipeline_options)
+
+# Lê os dados do arquivo 'casos_dengue.txt' e ignora a primeira linha (cabeçalho).
+dengue = (
+    pipeline
+    | "Leitura do Dataset dos casos de Dengue" >> ReadFromText('casos_dengue.txt', skip_header_lines=1)
+)
