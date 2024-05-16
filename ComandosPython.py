82e83588b92f7212cdfaa1543b8be184fbba7715
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-05-16 15:40:20 -0300

    Update ComandosPython.py

--- ComandosPython.py
+++ ComandosPython.py
@@ -9,8 +9,18 @@ pipeline_options = PipelineOptions(argv=None)
 # Inicializa o pipeline.
 pipeline = beam.Pipeline(options=pipeline_options)
 
+# Método com parâmetro para receber texto e um delimitador, retorna uma lista de elementos pelo delimitador
+def texto_para_lista(elemento, delimitador = '|') :
+    
+    return elemento.split(delimitador)
+
 # Lê os dados do arquivo 'casos_dengue.txt' e ignora a primeira linha (cabeçalho).
+
 dengue = (
     pipeline
     | "Leitura do Dataset dos casos de Dengue" >> ReadFromText('casos_dengue.txt', skip_header_lines=1)
+    | "De texto para lista" >> beam.Map(texto_para_lista)
+    | "Mostrar resultados" >> beam.Map(print)
 )
+
+pipeline.run()
