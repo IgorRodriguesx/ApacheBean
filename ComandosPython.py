c1f443c1d21ea8f1a7d3ace4a8d9bdbe143ee644
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-06-03 16:50:47 -0300

    Update ComandosPython.py

--- ComandosPython.py
+++ ComandosPython.py
@@ -25,71 +25,68 @@ colunas_dengue = [
 
 # Função para transformar uma lista em um dicionário usando as colunas definidas.
 def lista_para_dicionario(elemento, colunas):
-    """
-    Recebe uma lista e um conjunto de colunas
-    Retorna um dicionário com as colunas como chaves
-    """
     return dict(zip(colunas, elemento))
 
 # Método com parâmetro para receber texto e um delimitador, retorna uma lista de elementos pelo delimitador.
 def texto_para_lista(elemento, delimitador='|'):
+     """
+    Recebe 2 lista
+    Retorna 1 dicionário
     """
-    Recebe uma string e um delimitador
-    Retorna uma lista dos elementos divididos pelo delimitador
-    """
-    return elemento.split(delimitador)
+     return elemento.split(delimitador)
 
 def trata_datas(elemento):
     """
     Recebe um dicionário e cria um campo com ANO-MES
     Retorna o mesmo dicionário com o novo campo
     """
+    #"2016-08-01" >> ['2016','08'] >> "2016-08"
     elemento['ano_mes'] = '-'.join(elemento['data_iniSE'].split('-')[:2])
     return elemento
 
 def chave_uf(elemento):
     """
-    Recebe um dicionário
-    Retorna uma tupla com o estado (uf) e o elemento (uf, dicionario)
+    Receber um dicionário
+    Retornar uma tupla com o estado (uf) e o elemento (uf, dicionario)
     """
     chave = elemento['uf']
-    return (chave, elemento)
+    return (chave,elemento)
 
 def casos_dengue(elemento):
     """
     Recebe uma tupla ('RS', [{}, {}])
-    Retorna uma tupla ('RS-2014-12', 8.0)
+    Retornar uma tupla ('RS-2014-12', 8.0)
     """
-    # Desempacota a tupla de entrada ('RS', [{}, {}]).
+    # uf recebeu o estado (RS) e registros recebeu a array [{},{}] 
     uf, registros = elemento
-    
-    # Itera sobre os registros de casos de dengue.
+    # Percorrer a array (registros) usando o For e dando o nome de Registro
     for registro in registros:
-        # Verifica se o campo 'casos' contém dígitos.
         if bool(re.search(r'\d', registro['casos'])):
-            # Se o campo 'casos' contém dígitos, cria uma tupla com a chave composta (uf-ano_mes) e o valor dos casos como um float.
+        # Se fosse utilizado 'return' ele pararia no primeiro uf que achasse e dario o retorno, o yield faz percorrer todas as ufs
+        # f"" > para formatar o retorno, {} - {} para indicar de onde será tirado o retorno (Váriavel registro, no campo 'ano_mes')
             yield (f"{uf}-{registro['ano_mes']}", float(registro['casos']))
         else:
-            # Se o campo 'casos' não contém dígitos, cria uma tupla com a chave composta (uf-ano_mes) e o valor dos casos como 0.0.
             yield (f"{uf}-{registro['ano_mes']}", 0.0)
 
 # Leitura dos dados do arquivo 'casos_dengue.txt' e ignorando a primeira linha (cabeçalho).
-dengue = (
-    pipeline
-    | "Leitura do Dataset dos casos de Dengue" >> ReadFromText('casos_dengue.txt', skip_header_lines=1)
-    | "De texto para lista" >> beam.Map(texto_para_lista)
-    | "De lista para dicionário" >> beam.Map(lista_para_dicionario, colunas_dengue)
-    | "Criar campo ano_mes" >> beam.Map(trata_datas)
-    | "Criar chave pelo estado" >> beam.Map(chave_uf)
-    | "Agrupar pelo estado" >> beam.GroupByKey()
-    | "Descompactar casos de dengue" >> beam.FlatMap(casos_dengue)
-    | "Somar dos casos pela chave" >> beam.CombinePerKey(sum)
-    | "Mostrar resultados" >> beam.Map(print)
-)
+# dengue = (
+#     pipeline
+#     | "Leitura do Dataset dos casos de Dengue" >> ReadFromText('casos_dengue.txt', skip_header_lines=1)
+#     | "De texto para lista" >> beam.Map(texto_para_lista)
+#     | "De lista para dicionário" >> beam.Map(lista_para_dicionario, colunas_dengue)
+#     | "Criar campo ano_mes" >> beam.Map(trata_datas)
+#     | "Criar chave pelo estado" >> beam.Map(chave_uf)
+#     | "Agrupar pelo estado" >> beam.GroupByKey()
+#     | "Descompactar casos de dengue" >> beam.FlatMap(casos_dengue)
+#     | "Somar dos casos pela chave" >> beam.CombinePerKey(sum)
+#     | "Mostrar resultados" >> beam.Map(print)
+# )
 
 chuvas = (
     pipeline
     | "Leitura do Dataset de chuvas" >> ReadFromText('chuvas.csv', skip_header_lines=1)
+    | "De texto para lista (chuvas)" >> beam.Map(texto_para_lista, delimitador = ',')
+    | "Mostrar resultados de chuvas" >> beam.Map(print)
 )
 
 # Execução do pipeline.
